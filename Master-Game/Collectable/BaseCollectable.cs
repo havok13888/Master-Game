693beb9888fb44d5d78d0cbab295d285b4329eee
jamiahuswalton <jamiahuswalton@gmail.com>
2018-08-28 20:41:49 -0500

    Added a BaseCollectable class

---
+++ Master-Game/Collectable/BaseCollectable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterGame.Collectable
+{
+    class BaseCollectable
+    {
+        /// <summary>
+        /// Can be picked up.
+        /// </summary>
+        private bool isPickedUp;
+    }
+}
