ee958b6c26a0d21aed26dba2e622351f7d9152a0
jamiahuswalton <jamiahuswalton@gmail.com>
2018-09-01 14:50:48 -0500

    Added Json file

--- Master-Game/World/Map.cs
+++ Master-Game/World/Map.cs
@@ -1,20 +1,35 @@
-﻿using System.Drawing;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 
 namespace MasterGame.World
 {
     public class Map
     {
         private BaseTile [,] CurrentMap;
+        //private List<List<BaseTile>> MyCurrentMap;
 
         public void LoadMap()
         {
-            CurrentMap = new BaseTile [4,4]
+            MapData loadedMap = LoadJson();
+            int Rows = loadedMap.rows;
+            int Cols = loadedMap.cols;
+            //Tiles currentTile = loadedMap.tiles[i];
+
+
+            CurrentMap = new BaseTile[4,4]
             {
-                { new GrassTile(1, 1), new LavaTile(1, 1), new GrassTile(1, 1), new GrassTile(1, 1) },
-                { new GrassTile(1, 1), new LavaTile(1, 1), new WaterTile(1, 1), new GrassTile(1, 1) },
-                { new GrassTile(1, 1), new GrassTile(1, 1), new WaterTile(1, 1), new GrassTile(1, 1) },
-                { new GrassTile(1, 1), new GrassTile(1, 1), new GrassTile(1, 1), new GrassTile(1, 1) }   
+                { GetTile(loadedMap.tiles[0].Type), GetTile(loadedMap.tiles[1].Type), GetTile(loadedMap.tiles[2].Type), GetTile(loadedMap.tiles[3].Type) },
+                { GetTile(loadedMap.tiles[4].Type), GetTile(loadedMap.tiles[5].Type), GetTile(loadedMap.tiles[6].Type), GetTile(loadedMap.tiles[7].Type) },
+                { GetTile(loadedMap.tiles[8].Type), GetTile(loadedMap.tiles[9].Type), GetTile(loadedMap.tiles[10].Type), GetTile(loadedMap.tiles[11].Type) },
+                { GetTile(loadedMap.tiles[12].Type), GetTile(loadedMap.tiles[13].Type), GetTile(loadedMap.tiles[14].Type), GetTile(loadedMap.tiles[15].Type) }
             };
+
+
+
+           
         }
 
         public BaseTile TileAt(Point position)
@@ -37,6 +52,92 @@ namespace MasterGame.World
                 {
                     CurrentMap[x, y].Occupied = false;
                 }
+            }
+        }
+
+        public MapData LoadJson()
+        {
+            string fileName = "Jay01.json";
+            string filePath = Path.GetDirectoryName(Path.GetDirectoryName(System.IO.Directory.GetCurrentDirectory())) + "\\Assets\\Maps\\" + fileName;
+            FileInfo f = new FileInfo(fileName);
+            string fullName = f.Name;
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                string myJson = reader.ReadToEnd();
+                MapData mapData = JsonConvert.DeserializeObject<MapData>(myJson);
+                return mapData;
+            }
+        }
+
+        public class MapData
+        {
+            //Name
+            public string name { get; set; }
+            //Version
+            public string version { get; set; }
+            //row
+            public int rows { get; set; }
+            //col
+            public int cols { get; set; }
+            //Tiles
+            public Tiles [] tiles { get; set; }
+            //Entities
+            public Entities[] entities { get; set; }
+
+            public int GetRows()
+            {
+                return rows;
+            }
+        }
+
+        public class Tiles
+        {
+            [JsonProperty("type")]
+            public int Type;
+
+            [JsonProperty("x")]
+            public int xCor;
+
+            [JsonProperty("y")]
+            public int yCor;
+        }
+
+        public class Entities
+        {
+            [JsonProperty("type")]
+            public string Type;
+
+            [JsonProperty("name")]
+            public string Name;
+
+            [JsonProperty("healthPoints")]
+            public string HealthPoints;
+
+            [JsonProperty("x")]
+            public string xCor;
+
+            [JsonProperty("y")]
+            public string yCor;
+        }
+
+        private BaseTile GetTile(int typeNum)
+        {
+
+            if(TileType.Grass.GetHashCode() == typeNum)
+            {
+                return new GrassTile(1, 1);
+            } 
+            else if (TileType.Lava.GetHashCode() == typeNum)
+            {
+                return new LavaTile(1, 1);
+            } 
+            else if (TileType.Water.GetHashCode() == typeNum)
+            {
+                return new WaterTile(1, 1);
+            }
+            else
+            {
+                return null;
             }
         }
     }
