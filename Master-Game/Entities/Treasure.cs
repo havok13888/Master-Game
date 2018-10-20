8ffa955490a41e551754440224208d278593ac79
Unknown <lee.josh93@gmail.com>
2018-10-20 09:56:42 -0500

    Created New Treasure Entity
    Added Treasure class to entitities

---
+++ Master-Game/Entities/Treasure.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace MasterGame.Entities
+{
+    public class Treasure : BaseEntity
+    {
+        public int TreasureValue {get;}
+
+        public Treasure(int treasureValue) 
+            : base (EntityType.Treasure, "Treasure", 999, true, false, false, false)
+        {
+            TreasureValue = treasureValue;
+        }
+    }
+}
