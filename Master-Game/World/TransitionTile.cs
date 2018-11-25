06d4e89075ccfebebe47496ca844fabd9ff792dd
jamiahuswalton <jamiahuswalton@gmail.com>
2018-11-25 13:19:02 -0600

    Added class (Transition)

---
+++ Master-Game/World/TransitionTile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterGame.World
+{
+    class TransitionTile: BaseTile
+    {
+        private bool IsOpenForTransition { get; set; }
+
+        public TransitionTile(bool isOpenForTransition): base(TileType.Transition,true,0.0f)
+        {
+            IsOpenForTransition = isOpenForTransition;
+        }
+
+        // TODO: Need a method that was hold/pass the information and data needed for the next map. For example, if the player's health is 85 in the previousl map, it should be 80 in the next map
+        /// <summary>
+        /// This method will set the attribute for if the transition tile is open (i.e., = true)
+        /// </summary>
+        /// <param name="newCondition"></param>
+        public void SetIsOpenForTransition(bool newCondition)
+        {
+            IsOpenForTransition = newCondition;
+        }
+    }
+}
