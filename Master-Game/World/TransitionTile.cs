1995e245d80fc2f9593135f6e3411a07d4b4d9ff
jamiahuswalton <jamiahuswalton@gmail.com>
2018-12-15 11:42:10 -0600

    Simplfidied Transition tile code

--- Master-Game/World/TransitionTile.cs
+++ Master-Game/World/TransitionTile.cs
@@ -6,26 +6,12 @@ using System.Threading.Tasks;
 
 namespace MasterGame.World
 {
-    class TransitionTile: BaseTile
+    class TransitionTile : BaseTile
     {
-        private bool IsOpenForTransition;
+        private bool IsOpenForTransition { get; set;}
 
         public TransitionTile(): base(TileType.Transition,true,0.0f)
         {            
-        }
-
-        /// <summary>
-        /// This method will set the attribute for if the transition tile is open (i.e., = true)
-        /// </summary>
-        /// <param name="newCondition">True = Transition is Open</param>
-        public void SetIsOpenForTransition(bool newCondition)
-        {
-            IsOpenForTransition = newCondition;
-        }
-
-        public bool GetIsOpenForTransition()
-        {
-            return IsOpenForTransition;
         }
     }
 }
